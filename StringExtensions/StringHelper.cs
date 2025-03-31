14257b5c377bd0d02092542af4806a49683c7d4e
YogendraSherpunja <yogendramagar15@gmail.com>
2025-03-31 17:57:50 -0400

    string extenstion

---
+++ StringExtensions/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringExtensions
+{
+    public static class StringHelper
+    {
+        public static bool StartsWithUpper(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+
+            return char.IsUpper(value[0]);
+        }
+    }
+}
